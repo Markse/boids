670688ebdeb961a19d47440e1afe4b294e3da212
M. van Veen <markse@gmail.com>
2020-05-24 13:52:23 +0200

    Add debug option to BoidView

--- src/app.ts
+++ src/app.ts
@@ -15,15 +15,33 @@ class Main extends Phaser.Game {
 // tslint:disable-next-line: max-classes-per-file
 class BoidView extends Phaser.GameObjects.Graphics {
   boid: Boid;
+  debug: boolean;
 
   constructor(scene: Phaser.Scene, boid: Boid) {
     super(scene);
     this.scene.add.existing(this);
 
     this.boid = boid;
+    this.draw();
+  }
+
+  drawCircleSegment(angle: number, radius: number): void {
+    this.beginPath();
+    this.arc(0, 0, radius, -angle / 2, angle / 2);
+    this.lineTo(0, 0);
+    this.fillPath();
+  }
+  draw() {
+    if (this.debug) {
+      this.fillStyle(0xa0ffaf);
+      this.drawCircleSegment(Boid.sightAngle, Boid.sightRadius);
+      this.fillStyle(0xffafaf);
+      this.drawCircleSegment(Boid.sightAngle, Boid.repelRadius);
+    }
+
     this.fillStyle(0x00ffff);
-    this.fillTriangle(10, 0, -10, -7, -10, +7);
     this.lineStyle(1, 0x000000);
+    this.fillTriangle(10, 0, -10, -7, -10, +7);
     this.strokeTriangle(10, 0, -10, -7, -10, +7);
   }
 
@@ -56,6 +74,10 @@ class BoidScene extends Phaser.Scene {
       this.boids.push(boid);
       this.boidViews.push(new BoidView(this, boid));
     }
+
+    // Uncomment for debugging.
+    //this.boidViews[0].debug = true;
+    //this.boidViews[0].draw();
 
     this.cameras.main.setBackgroundColor("0xFFFFFF");
   }
